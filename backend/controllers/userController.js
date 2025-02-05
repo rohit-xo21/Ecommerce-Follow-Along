9fb5eefef169555161d43ce1f8ef149785c580e1
Rohit <rohithrajka524@gmail.com>
2025-02-05 10:48:13 +0530

    completed milestone 6

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,23 +3,25 @@ const path = require("path");
 const User = require("../model/user");
 const router = express.Router();
 const {upload} = require("../multer");
+const bcrypt = require('bcrypt');
 const ErrorHandler = require("../utils/ErrorHandler");
 
-router.post("/create-user",upload.single("file"), async(req,res)=>{
+router.post("/signup",upload.single("file"), async(req,res)=>{
     const {name, email, password} = req.body;
     const userEmail = await User.findOne({email});
     if (userEmail) {
         return next(new ErrorHandler("User already exists", 400));
     }
-const filename = req.file.filename ;
-const fileUrl = path.join(filename);
-const user={
-    name:name,
-    email:email,
-    password:password,
-    avatar: fileUrl,
-} ;
-console.log(user);
+    const filename = req.file.filename ;
+    const fileUrl = path.join(filename);
+    const hashedPassword = await bcrypt.hash(password,10);
+    const user={
+        name:name,
+        email:email,
+        password:hashedPassword,
+        avatar: fileUrl,
+    } ;
+    console.log(user);
 });
 
 module.exports = router;
